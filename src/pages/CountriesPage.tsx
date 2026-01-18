6d3a3d2eb1afed31db4b63237e437e0b859e86e6
Sunggin <sungginkim528@gmail.com>
2026-01-19 00:53:54 +0900

    attempt 1

--- src/pages/CountriesPage.tsx
+++ src/pages/CountriesPage.tsx
@@ -1,5 +1,5 @@
 import Select from "../components/ui/Select";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const BASE_URL = "https://restcountries.com/v3.1";
 
@@ -17,7 +17,6 @@ const FILTERABLE_CAPITALS = [
   "Rome",
 ] as const;
 
-
 type Capital = (typeof FILTERABLE_CAPITALS)[number];
 
 interface Country {
@@ -26,8 +25,46 @@ interface Country {
 }
 
 const CountriesPage = () => {
+  const [data, setData] = useState<String>("");
+  const [selected, setSelected] = useState("");
+
+  const handleClick = async () => {
+    const res = await fetch(`${BASE_URL}/capital/${selected}`);
+
+    const result = await res.json();
+    const country = result[0].name.common;
+    setData(() => country);
+  };
+
+  const handleChange = (e) => {
+    setSelected(() => e.target.value);
+  };
+
+  useEffect(() => {
+    handleClick();
+    console.log(selected);
+  }, [selected]);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
     <>
+      <Select
+        value={selected}
+        placeholder="Select a Capital"
+        onChange={handleChange}
+      >
+        {FILTERABLE_CAPITALS.map((capital, index) => (
+          <Select.Option
+            style={{ color: "black" }}
+            key={index}
+            value={capital}
+          />
+        ))}
+      </Select>
+      <p>{data}</p>
     </>
   );
 };
